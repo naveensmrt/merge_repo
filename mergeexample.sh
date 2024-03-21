38f115a5d1d00ec68ed9aebd437c9101659f4f4b
Ubuntu <ubuntu@ip-172-31-45-132.us-east-2.compute.internal>
2024-03-21 05:18:23 +0000

    merge commit 01
     main commit -02
    
    my main commit 03
    
    merge commit -02
    
    my merge commit -03
    
    my merge commit 4
    
     my merge commit -5
    
     main commit 4

--- mergeexample.sh
+++ mergeexample.sh
@@ -2,3 +2,38 @@
 
 
 #my main commit -01
+
+
+#my merge commit -01
+
+
+#my main commit - 02
+#my main commit - 03
+
+
+
+<<<<<<< HEAD
+#my merge commit -02
+#my merge commit -03
+
+
+
+=======
+>>>>>>> 8f4cdeb ( main commit 4)
+
+
+
+
+
+<<<<<<< HEAD
+
+
+
+
+
+
+#my merge commit - 4
+#my merge commit -5
+=======
+#my main commit -4
+>>>>>>> 8f4cdeb ( main commit 4)
